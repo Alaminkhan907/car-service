96990e10d84d19f56ca462d11e9bbb0a5a8a14bd
Alamin Khan <alamin@Alamins-Mini.lan>
2022-08-08 15:09:51 +0300

    first

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Experts from '../Experts/Experts.js'
+import Services from '../Services/Services.js'
+import Banner from '../Banner/Banner.js'
+const Home = () => {
+  return (
+    <>
+        <Banner></Banner>
+        <Services></Services>
+        <Experts></Experts>
+    </>
+  )
+}
+
+export default Home
