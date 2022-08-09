7cdaae7c0cd89a700c839781cad447dd75b911c0
Alamin Khan <alamin@Alamins-Mini.lan>
2022-08-09 15:00:44 +0300

    login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -2,56 +2,70 @@ import React from 'react'
 import { useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
-    const location = useLocation();
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    let from = location.state?.from?.pathname || "/";
+  let from = location.state?.from?.pathname || "/";
+  let errorElement;
 
-    const [
-        signInWithEmailAndPassword,
-        user,
-        loading,
-        error,
-      ] = useSignInWithEmailAndPassword(auth);
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
 
-        if(user){
-            navigate(from, { replace: true });
-        }
-    const handleSubmit = event =>{
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        signInWithEmailAndPassword(email, password);
-    }
-    const navigateRegister = event =>{
-        navigate('/register');
-    }
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+  if (error) {
+
+    errorElement = <p className='text-danger'>Error: {error.message} </p>
+
+
+  }
+  const handleSubmit = event => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
+  }
+  const navigateRegister = event => {
+    navigate('/register');
+  }
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    await sendPasswordResetEmail(email);
+    alert('Sent email');
+  }
   return (
     <div className='container w-50 mx-auto'>
-        <h1 className='text-primary text-center mt-2'>Please Login</h1>
-        <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-        
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    <p>New to Car service? <Link to='/register' className='text-danger text-decoration-none' onClick={navigateRegister}>Please register</Link></p>
+      <h1 className='text-primary text-center mt-2'>Please Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+        </Form.Group>
+        <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+          Login
+        </Button>
+      </Form>
+      {errorElement}
+      <p>New to Car service? <Link to='/register' className='text-primary text-decoration-none' onClick={navigateRegister}>Please register</Link></p>
+      <p>Forget password ? <Link to='/register' className='text-primary text-decoration-none' onClick={resetPassword}>Reset password</Link></p>
+      <SocialLogin></SocialLogin>
     </div>
   )
 }
